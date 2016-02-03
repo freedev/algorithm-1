9efd80183ab58e7c63a4350b93d19fa506ab1fa2
Vincenzo D'Amore <v.damore@gmail.com>
2016-02-03 01:26:08 +0100

    98.5

--- week-1/percolation/src/Percolation.java
+++ week-1/percolation/src/Percolation.java
@@ -14,8 +14,9 @@ public class Percolation {
 
     // where (1, 1) is the upper-left site
     private WeightedQuickUnionUF mapUnion;
+    private WeightedQuickUnionUF mapFully;
     // percolation map
-    private int[] map;
+    private char[] map;
     // size of the map
     private int size;
     // a site within first row
@@ -37,9 +38,10 @@ public class Percolation {
             throw new IllegalArgumentException();
         }
         size = N;
-        map = new int[N * N];
+        map = new char[N * N];
         // Weighted union-find data type
         mapUnion = new WeightedQuickUnionUF(N * N + 2);
+        mapFully = new WeightedQuickUnionUF(N * N);
         // defaults to -1
         firstRowPos = N * N;
         lastRowPos = N * N + 1;
@@ -48,41 +50,41 @@ public class Percolation {
     /**
      * helper method - translate row and column into position
      * 
-     * @param i
+     * @param row
      *            row {@code int}
-     * @param j
+     * @param col
      *            column {@code int}
      * @return
      */
-    private int getPos(int i, int j) {
-        return (--i * size + --j);
+    private int getPos(int row, int col) {
+        return (--row * size + --col);
     }
 
     /**
      * check if a site at row and column is OPENED
      * 
-     * @param i
+     * @param row
      *            row {@code int}
-     * @param j
+     * @param col
      *            column {@code int}
      * @return {@code boolean} returns true if site at row and column is OPENED
      */
-    public boolean isOpen(int i, int j) {
-        validateBounds(i, j);
-        return (map[getPos(i, j)] == OPENED);
+    public boolean isOpen(int row, int col) {
+        validateBounds(row, col);
+        return (map[getPos(row, col)] != CLOSED);
     }
 
     /**
      * helper method - validate row and column, throws if row or column are not
      * valid.
      * 
-     * @param i
+     * @param row
      *            row {@code int}
-     * @param j
+     * @param col
      *            column {@code int}
      */
-    private void validateBounds(int i, int j) {
-        if (i < 1 || j < 1 || i > size || j > size) {
+    private void validateBounds(int row, int col) {
+        if (row < 1 || col < 1 || row > size || col > size) {
             throw new IndexOutOfBoundsException();
         }
     }
@@ -98,6 +100,7 @@ public class Percolation {
      */
     public void open(int row, int col) {
         validateBounds(row, col);
+
         int pos = getPos(row, col);
         if (map[pos] == CLOSED) {
 
@@ -105,6 +108,7 @@ public class Percolation {
 
             if (row == 1) {
                 mapUnion.union(pos, firstRowPos);
+                mapFully.union(pos, firstRowPos);
             }
             if (row == size) {
                 mapUnion.union(pos, lastRowPos);
@@ -112,23 +116,30 @@ public class Percolation {
 
             // Left
             if (col > 1) {
-                if (map[pos - 1] != CLOSED)
+                if (map[pos - 1] != CLOSED) {
                     mapUnion.union(pos, pos - 1);
+                    mapFully.union(pos, pos - 1);
+                }
             }
             // Right
             if (col < size) {
-                if (map[pos + 1] != CLOSED)
+                if (map[pos + 1] != CLOSED) {
                     mapUnion.union(pos, pos + 1);
+                    mapFully.union(pos, pos + 1);
+                }
             }
             // Up
             if (row > 1) {
-                if (map[pos - size] != CLOSED)
+                if (map[pos - size] != CLOSED) {
                     mapUnion.union(pos, pos - size);
+                    mapFully.union(pos, pos - size);
+                }
             }
             // Down
             if (row < size) {
                 if (map[pos + size] != CLOSED) {
                     mapUnion.union(pos, pos + size);
+                    mapFully.union(pos, pos + size);
                 }
             }
 
@@ -138,17 +149,17 @@ public class Percolation {
     /**
      * Check if a site at row and column is FULL
      * 
-     * @param i
+     * @param row
      *            row {@code int}
-     * @param j
+     * @param col
      *            column {@code int}
      * @return boolean - returns true if site state is FULL
      */
-    public boolean isFull(int i, int j) {
-        validateBounds(i, j);
-        int pos = getPos(i, j);
-        if (map[pos] == OPENED && mapUnion.connected(pos, firstRowPos)) {
-              map[pos] = FULL;
+    public boolean isFull(int row, int col) {
+        validateBounds(row, col);
+        int pos = getPos(row, col);
+        if (map[pos] == OPENED && mapFully.connected(pos, firstRowPos)) {
+            map[pos] = FULL;
         }
         if (map[pos] == FULL)
             return true;
