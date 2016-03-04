66d4b6e10de77da365a0864045b26f0ff56af66e
Vincenzo D'Amore <v.damore@gmail.com>
2016-03-04 11:15:37 +0100

    test using binarysearch to avoid duplicates

--- week-3/collinear/src/FastCollinearPoints.java
+++ week-3/collinear/src/FastCollinearPoints.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-import java.util.TreeSet;
 
 /**
  * 
@@ -10,20 +9,197 @@ public class FastCollinearPoints {
 
     private Point[] points;
     private LineSegment[] segments;
+    private int segmentSize;
 
+    private class BinarySearchST {
+        private static final int INIT_CAPACITY = 2;
+        private PairOfPoints[] keys;
+        private LineSegment[] vals;
+        private int N = 0;
+        
+        /**
+         * Initializes an empty symbol table.
+         */
+        public BinarySearchST() {
+            this(INIT_CAPACITY);
+        }
+
+        /**
+         * Initializes an empty symbol table with the specified initial capacity.
+         */
+        public BinarySearchST(int capacity) { 
+            keys = new PairOfPoints[capacity]; 
+            vals = new LineSegment[capacity]; 
+        }   
+
+        // resize the underlying arrays
+        private void resize(int capacity) {
+            assert capacity >= N;
+            PairOfPoints[]   tempk = new PairOfPoints[capacity];
+            LineSegment[] tempv = new LineSegment[capacity];
+            for (int i = 0; i < N; i++) {
+                tempk[i] = keys[i];
+                tempv[i] = vals[i];
+            }
+            vals = tempv;
+            keys = tempk;
+        }
+
+        /**
+         * Returns the number of key-value pairs in this symbol table.
+         *
+         * @return the number of key-value pairs in this symbol table
+         */
+        public int size() {
+            return N;
+        }
+
+        /**
+         * Returns true if this symbol table is empty.
+         *
+         * @return <tt>true</tt> if this symbol table is empty;
+         *         <tt>false</tt> otherwise
+         */
+        public boolean isEmpty() {
+            return size() == 0;
+        }
+
+
+        /**
+         * Does this symbol table contain the given key?
+         *
+         * @param  key the key
+         * @return <tt>true</tt> if this symbol table contains <tt>key</tt> and
+         *         <tt>false</tt> otherwise
+         * @throws NullPointerException if <tt>key</tt> is <tt>null</tt>
+         */
+        public boolean contains(PairOfPoints key) {
+            if (key == null) throw new NullPointerException("argument to contains() is null");
+            return get(key) != null;
+        }
+
+        /**
+         * Returns the value associated with the given key in this symbol table.
+         *
+         * @param  key the key
+         * @return the value associated with the given key if the key is in the symbol table
+         *         and <tt>null</tt> if the key is not in the symbol table
+         * @throws NullPointerException if <tt>key</tt> is <tt>null</tt>
+         */
+        public LineSegment get(PairOfPoints key) {
+            if (key == null) throw new NullPointerException("argument to get() is null"); 
+            if (isEmpty()) return null;
+            int i = rank(key); 
+            if (i < N && keys[i].compareTo(key) == 0) return vals[i];
+            return null;
+        } 
+
+        /**
+         * Returns the number of keys in this symbol table strictly less than <tt>key</tt>.
+         *
+         * @param  key the key
+         * @return the number of keys in the symbol table strictly less than <tt>key</tt>
+         * @throws NullPointerException if <tt>key</tt> is <tt>null</tt>
+         */
+        public int rank(PairOfPoints key) {
+            if (key == null) throw new NullPointerException("argument to rank() is null"); 
+
+            int lo = 0, hi = N-1; 
+            while (lo <= hi) { 
+                int mid = lo + (hi - lo) / 2; 
+                int cmp = key.compareTo(keys[mid]);
+                if      (cmp < 0) hi = mid - 1; 
+                else if (cmp > 0) lo = mid + 1; 
+                else return mid; 
+            } 
+            return lo;
+        } 
+
+
+        /**
+         * Removes the specified key and its associated value from this symbol table
+         * (if the key is in this symbol table).
+         *
+         * @param  key the key
+         * @param  val the value
+         * @throws NullPointerException if <tt>key</tt> is <tt>null</tt>
+         */
+        public void put(PairOfPoints key, LineSegment val)  {
+            if (key == null) throw new NullPointerException("first argument to put() is null"); 
+
+            if (val == null) {
+                delete(key);
+                return;
+            }
+
+            int i = rank(key);
+
+            // key is already in table
+            if (i < N && keys[i].compareTo(key) == 0) {
+                vals[i] = val;
+                return;
+            }
+
+            // insert new key-value pair
+            if (N == keys.length) resize(2*keys.length);
+
+            for (int j = N; j > i; j--)  {
+                keys[j] = keys[j-1];
+                vals[j] = vals[j-1];
+            }
+            keys[i] = key;
+            vals[i] = val;
+            N++;
+
+        } 
+
+        /**
+         * Removes the specified key and associated value from this symbol table
+         * (if the key is in the symbol table).
+         *
+         * @param  key the key
+         * @throws NullPointerException if <tt>key</tt> is <tt>null</tt>
+         */
+        public void delete(PairOfPoints key) {
+            if (key == null) throw new NullPointerException("argument to delete() is null"); 
+            if (isEmpty()) return;
+
+            // compute rank
+            int i = rank(key);
+
+            // key not in table
+            if (i == N || keys[i].compareTo(key) != 0) {
+                return;
+            }
+
+            for (int j = i; j < N-1; j++)  {
+                keys[j] = keys[j+1];
+                vals[j] = vals[j+1];
+            }
+
+            N--;
+            keys[N] = null;  // to avoid loitering
+            vals[N] = null;
+
+            // resize if 1/4 full
+            if (N > 0 && N == keys.length/4) resize(keys.length/2);
+
+        } 
+
+
+    }
+    
+    /**
+     * 
+     * @author freedev
+     *
+     */
     private class PairOfPoints implements Comparable<PairOfPoints> {
         private Point start;
         private Point end;
         public PairOfPoints(Point s, Point e) {
             this.start = s;
             this.end = e;
-        }
-        public int compare(PairOfPoints o1, PairOfPoints o2) {
-            // TODO Auto-generated method stub
-            if (o1.start.compareTo(o2.start) == 0)
-                return o1.end.compareTo(o2.end);
-            else
-                return o1.start.compareTo(o2.start);
         }
         @Override
         public int compareTo(PairOfPoints o) {
@@ -40,17 +216,34 @@ public class FastCollinearPoints {
      * @param points
      */
     public FastCollinearPoints(Point[] points) {
-        if (points == null) {
+        loadPoints(points);
+        checkForDuplicates();
+        if (segments == null) {
+            buildSegments(points);
+        }
+    }
+
+    /**
+     * @param args
+     */
+    private void loadPoints(Point[] args) {
+        if (args == null) {
             throw new java.lang.NullPointerException();
         }
-        this.points = new Point[points.length];
-        for (int i = 0; i < points.length; i++) {
-            if (points[i] == null) {
+        this.points = new Point[args.length];
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] == null) {
                 throw new java.lang.NullPointerException();
             } else {
-                this.points[i] = points[i];
+                this.points[i] = args[i];
             }
         }
+    }
+
+    /**
+     * 
+     */
+    private void checkForDuplicates() {
         if (this.points.length > 1) {
             Arrays.sort(this.points);
             for (int i = 1; i < this.points.length; i++) {
@@ -58,14 +251,16 @@ public class FastCollinearPoints {
                     throw new java.lang.IllegalArgumentException();
             }
         }
-        if (segments == null) {
-            buildSegments(points);
-        }
     }
+    
+    /**
+     * 
+     * @param givenPoints
+     */
 
     private void buildSegments(Point[] givenPoints) {
         if (segments == null) {
-            TreeSet<PairOfPoints> segmentSet = new TreeSet<>();
+            BinarySearchST segmentSet = new BinarySearchST();
             for (int i = 0; i < givenPoints.length; i++) {
                 Arrays.sort(this.points, givenPoints[i].slopeOrder());
                 // StdOut.println("-");
@@ -95,18 +290,15 @@ public class FastCollinearPoints {
                         Arrays.sort(result);
                         PairOfPoints l = new PairOfPoints(result[0], result[result.length-1]);
                         if (!segmentSet.contains(l)) {
-                            segmentSet.add(l);
+                            segmentSet.put(l, new LineSegment(l.start, l.end));;
                         }
                     }
                     j++;
                  }
             }
-            segments = new LineSegment[segmentSet.size()];
-            int i = 0;
-            for (PairOfPoints pp : segmentSet) {
-                segments[i] = new LineSegment(pp.start, pp.end);
-                i++;
-            }
+            segmentSize = segmentSet.size();
+            segments = segmentSet.vals;
+            segmentSet.vals = null;
         }
     }
 
@@ -115,7 +307,7 @@ public class FastCollinearPoints {
      * @return
      */
     public int numberOfSegments() {
-        return segments.length;
+        return segmentSize;
     }
 
     /**
@@ -123,9 +315,10 @@ public class FastCollinearPoints {
      * @return
      */
     public LineSegment[] segments() {
-        LineSegment[] s = new LineSegment[segments.length];
+        LineSegment[] s = new LineSegment[segmentSize];
         for (int i = 0; i < segments.length; i++) {
-            s[i] = segments[i];
+            if (segments[i] != null)
+                s[i] = segments[i];
         }
 
         return s;
