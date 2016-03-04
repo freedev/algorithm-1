7ee8562a81a4096169a064b8470e2b129169fab2
Vincenzo D'Amore <v.damore@gmail.com>
2016-03-04 10:02:00 +0100

    collinear 97.20

--- week-3/collinear/src/FastCollinearPoints.java
+++ week-3/collinear/src/FastCollinearPoints.java
@@ -1,6 +1,4 @@
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.TreeSet;
 
 /**
@@ -13,6 +11,30 @@ public class FastCollinearPoints {
     private Point[] points;
     private LineSegment[] segments;
 
+    private class PairOfPoints implements Comparable<PairOfPoints> {
+        private Point start;
+        private Point end;
+        public PairOfPoints(Point s, Point e) {
+            this.start = s;
+            this.end = e;
+        }
+        public int compare(PairOfPoints o1, PairOfPoints o2) {
+            // TODO Auto-generated method stub
+            if (o1.start.compareTo(o2.start) == 0)
+                return o1.end.compareTo(o2.end);
+            else
+                return o1.start.compareTo(o2.start);
+        }
+        @Override
+        public int compareTo(PairOfPoints o) {
+            // TODO Auto-generated method stub
+            if (this.start.compareTo(o.start) == 0)
+                return this.end.compareTo(o.end);
+            else
+                return this.start.compareTo(o.start);
+        }
+    }
+    
     /**
      * 
      * @param points
@@ -43,30 +65,27 @@ public class FastCollinearPoints {
 
     private void buildSegments(Point[] givenPoints) {
         if (segments == null) {
-            TreeSet<String> segmentSet = new TreeSet<>();
-            List<LineSegment> lsegments = new LinkedList<>();
+            TreeSet<PairOfPoints> segmentSet = new TreeSet<>();
             for (int i = 0; i < givenPoints.length; i++) {
                 Arrays.sort(this.points, givenPoints[i].slopeOrder());
-//                StdOut.println("-");
-//                for (int j = 0; j < this.points.length; j++) {
-//                    StdOut.print(this.points[j]);
-//                    StdOut.print("-");
-//                }
-//                StdOut.println("-");
-                for (int j = 1; j < this.points.length - 2; j++) {
+                // StdOut.println("-");
+                // for (int j = 0; j < this.points.length; j++) {
+                // StdOut.print(this.points[j]);
+                // StdOut.print("-");
+                // }
+                // StdOut.println("-");
+                int j = 1;
+                while (j < (this.points.length - 2)) {
                     double s = this.points[0].slopeTo(this.points[j]);
                     int k = 0;
-                    boolean found = false;
                     while ((j + k) < this.points.length - 1) {
-                        if (s == this.points[0]
+                        if (s != this.points[0]
                                 .slopeTo(this.points[j + k + 1])) {
-                            found = true;
-                        } else {
                             break;
                         }
                         k++;
                     }
-                    if (found && k > 1) {
+                    if (k > 1) {
                         Point[] result = new Point[k + 2];
                         result[0] = this.points[0];
                         for (int jj = 0; jj < k + 1; jj++) {
@@ -74,19 +93,19 @@ public class FastCollinearPoints {
                         }
                         j = j + k + 1;
                         Arrays.sort(result);
-                        if (!segmentSet.contains(result[0].toString()
-                                + result[result.length-1].toString())) {
-                            segmentSet.add(result[0].toString()
-                                    + result[result.length-1].toString());
-                            lsegments.add(
-                                    new LineSegment(result[0], result[result.length-1]));
+                        PairOfPoints l = new PairOfPoints(result[0], result[result.length-1]);
+                        if (!segmentSet.contains(l)) {
+                            segmentSet.add(l);
                         }
                     }
-                }
+                    j++;
+                 }
             }
-            segments = new LineSegment[lsegments.size()];
-            for (int i = 0; i < segments.length; i++) {
-                segments[i] = lsegments.remove(0);
+            segments = new LineSegment[segmentSet.size()];
+            int i = 0;
+            for (PairOfPoints pp : segmentSet) {
+                segments[i] = new LineSegment(pp.start, pp.end);
+                i++;
             }
         }
     }
